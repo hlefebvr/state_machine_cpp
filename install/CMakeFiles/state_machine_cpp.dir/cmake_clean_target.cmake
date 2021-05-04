0bbdaa9dc31726fc5321272c5d072135ab6336af
Henri Lefebvre <hri.lefebvre@gmail.com>
2021-05-04 12:15:34 +0200

    remove install folder

--- install/CMakeFiles/state_machine_cpp.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libstate_machine_cpp.a"
-)
--- install/CMakeFiles/state_machine_cpp.dir/state_machine_cpp/src/transitions/transition_handlers.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/state_machine_cpp.dir/state_machine_cpp/src/transitions/transition_handlers.cpp.o: \
- /home/henri/CLionProjects/state_machine_cpp/state_machine_cpp/src/transitions/transition_handlers.cpp \
- /usr/include/stdc-predef.h \
- /home/henri/CLionProjects/state_machine_cpp/state_machine_cpp/include/transitions/transition_handlers.h
